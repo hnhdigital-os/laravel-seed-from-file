417c4e8f9a5aa6559fa54e147dd384f7921e2fdf
Lucas Mezêncio <lucas.mezencio@gmail.com>
2017-02-20 22:51:41 -0300

    Reorganise file and change to camelCase

--- src/SeedFromFileCommand.php
+++ src/SeedFromFileCommand.php
@@ -13,12 +13,12 @@ class SeedFromFileCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'db:seed-from-file {dir} {force_import?}';
+    protected $signature = 'db:seed-from-file {dir} {force_import?} {connection?}';
 
     /**
      * The console command description.
      *
-     * @var strings
+     * @var string
      */
     protected $description = 'Seed database from a file or files in a directory.';
 
@@ -30,7 +30,7 @@ class SeedFromFileCommand extends Command
     public function handle()
     {
         $directory = $this->argument('dir');
-        $force_import = !empty($this->argument('force_import'));
+        $forceImport = !empty($this->argument('force_import'));
 
         try {
             $type = File::type($directory);
@@ -47,52 +47,53 @@ class SeedFromFileCommand extends Command
             if ($directory[strlen($directory) - 1] === '/') {
                 $directory = substr($directory, 0, -1);
             }
+
             $files = File::files($directory);
         } else {
             $files = [$directory];
         }
 
-        $progress_bar = $this->output->createProgressBar(count($files));
+        $progressBar = $this->output->createProgressBar(count($files));
+        $filesOrder = [];
+        $noOrder = count($files);
 
-        $files_order = [];
+        foreach ($files as $filePath) {
+            $tableName = File::name($filePath);
+            $tableNameArray = explode('_', $tableName, 2);
 
-        $no_order = count($files);
-
-        foreach ($files as $file_path) {
-            $table_name = File::name($file_path);
-
-            $table_name_array = explode('_', $table_name, 2);
-            if (is_numeric($table_name_array[0])) {
-                $table_order = $table_name_array[0];
-                $files_order[$table_order] = $file_path;
+            if (is_numeric($tableNameArray[0])) {
+                $tableOrder = $tableNameArray[0];
+                $filesOrder[$tableOrder] = $filePath;
             } else {
-                $files_order[$no_order] = $file_path;
-                $no_order++;
+                $filesOrder[$noOrder] = $filePath;
+                $noOrder++;
             }
         }
 
-        ksort($files_order);
+        ksort($filesOrder);
 
-        foreach ($files_order as $file_path) {
-            $table_name = File::name($file_path);
+        foreach ($filesOrder as $filePath) {
+            $tableName = File::name($filePath);
+            $tableNameArray = explode('_', $tableName, 2);
 
-            $table_name_array = explode('_', $table_name, 2);
-            if (is_numeric($table_name_array[0])) {
-                $table_name = $table_name_array[1];
+            if (is_numeric($tableNameArray[0])) {
+                $tableName = $tableNameArray[1];
             }
 
-            $total_records = DB::table($table_name)->select(DB::raw('count(*) as total_records'))->value('total_records');
+            $totalRecords = DB::table($tableName)
+                ->select(DB::raw('count(*) as total_records'))->value('total_records');
 
-            if ($total_records === 0 || $force_import) {
+            if ($totalRecords === 0 || $forceImport) {
                 try {
-                    DB::unprepared(File::get($file_path));
+                    DB::unprepared(File::get($filePath));
                 } catch (\Exception $exception) {
                     $this->info('');
-                    $this->error('SQL error occured on importing '.$table_name);
+                    $this->error('SQL error occured on importing '.$tableName);
                     $this->info('');
                 }
             }
-            $progress_bar->advance();
+
+            $progressBar->advance();
         }
 
         $this->info('');
